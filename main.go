122e75cb1bd26508f18010d41f0fdef16ccddf46
Chamila de Alwis <chamila@apache.org>
2017-04-17 17:26:37 +0530

    Forked process

--- main.go
+++ main.go
@@ -10,24 +10,37 @@ import (
 func main() {
 	switch os.Args[1] {
 	case "run":
+		// isolated process
 		run()
+	case "child":
+		// don't fork
+		child()
 	default:
 		panic("What?")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2])
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		// new namespace and process id for my command
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("running [%v] as PID %v\n", os.Args[2], os.Getpid())
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	// new namespace for my command
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
-	}
 
 	must(cmd.Run())
 }
